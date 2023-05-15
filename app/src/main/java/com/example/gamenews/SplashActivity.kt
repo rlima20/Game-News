034078b70ddaaeb71ecc9875a757581644493437
CIANDT\raphaellima <raphael.lima-ext@ab-inbev.com>
2023-05-14 22:28:18 -0300

    Launch main activity and new hexa colors

--- app/src/main/java/com/example/gamenews/SplashActivity.kt
+++ app/src/main/java/com/example/gamenews/SplashActivity.kt
@@ -1,27 +1,45 @@
 package com.example.gamenews
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import com.example.gamenews.ui.theme.GameNewsTheme
 
+@Suppress("DEPRECATION")
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java).apply {}
+            startActivity(intent)
+        }, 2000)
+
         setContent {
             GameNewsTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    color = MaterialTheme.colors.background,
                 ) {
-                    Text("Splash Activity")
+                    Image(
+                        modifier = Modifier.background(
+                            color = colorResource(id = R.color.splash_activity_main_color)
+                        ),
+                        painter = painterResource(id = R.drawable.logo),
+                        contentDescription = null
+                    )
                 }
             }
         }
